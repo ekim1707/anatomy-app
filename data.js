ef3df0d3f065f908796a615b8f495401f02198a1
Eugene Kim <ekim1707@gmail.com>
2019-11-07 14:07:10 -0500

    updated media player

---
+++ data.js
@@ -0,0 +1,36 @@
+const articleData = [{
+    name: 'Time Spent in Nature is Good For You',
+    resource: 'TheScientist',
+    date: 'Oct 2019',
+    url: 'nature.pdf',
+    saved: false
+},
+{
+    name: 'Martha Muñoz Uncovers the Drivers and Dampers of Biodiversity',
+    resource: 'TheScientist',
+    date: 'Nov 2019',
+    url: 'marthamunoz.pdf',
+    saved: false
+}]
+
+const videoData = [{
+    name: 'Carnival Scam Science',
+    resource: 'Mark Rober',
+    date: 'Oct 2017',
+    url: 'https://www.youtube.com/embed/tk_ZlWJ3qJI',
+    saved: false
+}]
+
+const imageData = [{
+    name: 'Internal Upper Body View',
+    resource: 'University of Western Australia',
+    date: '',
+    url: 'upperbody.jpg',
+    saved: false
+}]
+
+module.exports = {
+    articleData,
+    videoData,
+    imageData
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const data = require('../data');
+const articleLis = data.articleData.map(article => [{name: article.name, resource: article.resource, date: article.date}]);
+const articleURLs = data.articleData.map(article => article.url);
+const savedArticles = data.articleData.filter(article => article.saved);
+const videoLis = data.videoData.map(video =>[{name: video.name, resource: video.resource, date: video.date}]);
+const videoURLs = data.videoData.map(video => video.url);
+const savedVideos = data.videoData.filter(video => video.saved);
+const imageLis = data.imageData.map(image => [{name: image.name, resource: image.resource, date: image.date}]);
+const imageURLs = data.imageData.map(image => image.url);
+const savedImages = data.imageData.filter(image => image.saved);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Express',
+    articleLis: articleLis,
+    articleURLs: articleURLs,
+    savedArticles: savedArticles,
+    videoLis: videoLis,
+    videoURLs: videoURLs,
+    savedVideos: savedVideos,
+    imageLis: imageLis,
+    imageURLs: imageURLs,
+    savedImages: savedImages
+  });
 });
 
 module.exports = router;
