b3e6a92a2a6b7cb2e244b4696d171babb48270ef
Eugene Kim <ekim1707@gmail.com>
2019-11-08 09:27:15 -0500

    updates

--- data.js
+++ data.js
@@ -11,6 +11,48 @@ const articleData = [{
     date: 'Nov 2019',
     url: 'marthamunoz.pdf',
     saved: false
+},
+{
+    name: 'Phage therapy: An alternative to antibiotics in the age of multi-drug resistance',
+    resource: 'NCBI',
+    date: 'Aug 2017',
+    url: 'phagetherapy.pdf',
+    saved: false
+},
+{
+    name: 'Light leaking from a distant galaxy hints at a cosmic makeover’s origins',
+    resource: 'ScienceNews',
+    date: 'Nov 2019',
+    url: 'cosmicorigins.pdf',
+    saved: false
+},
+{
+    name: 'Trapping atoms in a laser beam offers a new way to measure gravity',
+    resource: 'ScienceNews',
+    date: 'Nov 2019',
+    url: 'measuregravity.pdf',
+    saved: false
+},
+{
+    name: 'Self-destructing mitochondria may leave some brain cells vulnerable to ALS',
+    resource: 'ScienceNews',
+    date: 'Nov 2019',
+    url: 'selfdestructmito.pdf',
+    saved: false
+},
+{
+    name: 'Fake Sunflowers Could Harvest Solar Energy',
+    resource: 'Popular Mechanics',
+    date: 'Nov 2019',
+    url: 'sunflowers.pdf',
+    saved: false
+},
+{
+    name: '11,000+ Scientists Declare Climate Emergency',
+    resource: 'Popular Mechanics',
+    date: 'Nov 2019',
+    url: 'climateemergency.pdf',
+    saved: false
 }]
 
 const videoData = [{
@@ -19,6 +61,66 @@ const videoData = [{
     date: 'Oct 2017',
     url: 'https://www.youtube.com/embed/tk_ZlWJ3qJI',
     saved: false
+},{
+    name: 'Introduction to Anatomy & Physiology: Crash Course A&P #1',
+    resource: 'CrashCourse',
+    date: 'Jan 2015',
+    url: 'https://www.youtube.com/embed/uBGl2BujkPQ',
+    saved: false
+},{
+    name: 'Introduction: Neuroanatomy Video Lab - Brain Dissections',
+    resource: 'Eccles Health Sciences Library Digital Publishing',
+    date: 'Jul 2015',
+    url: 'https://www.youtube.com/embed/_aCCsRCw78g',
+    saved: false
+},{
+    name: 'THE HUMAN BONES SONG',
+    resource: 'Jam Campus',
+    date: 'Oct 2018',
+    url: 'https://www.youtube.com/embed/h5dYvPruBFY',
+    saved: false
+},{
+    name: 'The 6 Types of Joints - Human Anatomy for Artists',
+    resource: 'Proko',
+    date: 'Dec 2014',
+    url: 'https://www.youtube.com/embed/0cYal_hitz4',
+    saved: false
+},{
+    name: 'Inflammatory response',
+    resource: 'Khan Academy',
+    date: 'Feb 2010',
+    url: 'https://www.youtube.com/embed/FXSuEIMrPQk',
+    saved: false
+},{
+    name: 'Lung Anatomy',
+    resource: 'Nucleus Medical Media',
+    date: 'March 2011',
+    url: 'https://www.youtube.com/embed/Aw9OJLTlClQ',
+    saved: false
+},{
+    name: 'Anatomy and Physiology of The Heart',
+    resource: 'New Anatomy and Physiology Video',
+    date: 'Nov 2015',
+    url: 'https://www.youtube.com/embed/Mv3dKgwbJ7U',
+    saved: false
+},{
+    name: 'What makes Tyreek Hill the fastest player in the NFL?',
+    resource: 'ESPN',
+    date: 'Oct 2017',
+    url: 'https://www.youtube.com/embed/aiU4xQ7ICM4',
+    saved: false
+},{
+    name: 'Ray Lewis Sport Science',
+    resource: 'ReySkin',
+    date: 'Apr 2009',
+    url: 'https://www.youtube.com/embed/u1CwwsWkQLU',
+    saved: false
+},{
+    name: 'Odell Beckham Jr.‘s one-handed catch',
+    resource: 'ESPN',
+    date: 'Nov 2017',
+    url: 'https://www.youtube.com/embed/_UlcOh_J4dY',
+    saved: false
 }]
 
 const imageData = [{
@@ -26,6 +128,48 @@ const imageData = [{
     resource: 'University of Western Australia',
     date: '',
     url: 'upperbody.jpg',
+    saved: false
+},{
+    name: 'What is physiology?',
+    resource: 'physoc.org',
+    date: '',
+    url: 'physiology.jpg',
+    saved: false
+},{
+    name: 'Difference between children and adults',
+    resource: `The Royal Children's Hospital Melbourne`,
+    date: '',
+    url: 'children.jpg',
+    saved: false
+},{
+    name: 'Cardiac System',
+    resource: 'The Physiological Society',
+    date: '',
+    url: 'heart.jpg',
+    saved: false
+},{
+    name: 'Human Physiology Diagram',
+    resource: 'Pinterest',
+    date: '',
+    url: 'physiologydiagram.jpg',
+    saved: false
+},{
+    name: 'Internal Organs',
+    resource: '123rf.com',
+    date: '',
+    url: 'internalorgans.jpg',
+    saved: false
+},{
+    name: 'Frontal view of internal human body',
+    resource: 'The School of Natural Therapies',
+    date: '',
+    url: 'frontalbody.jpg',
+    saved: false
+},{
+    name: 'Our Body... Our Machine',
+    resource: 'FitRanX',
+    date: '',
+    url: 'ourmachine.jpg',
     saved: false
 }]
 
